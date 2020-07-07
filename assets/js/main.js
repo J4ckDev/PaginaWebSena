d48cb9a4a081e7a63fdf6b208e7ef2206a129099
J4ckDev <j4ckdev@gmail.com>
2020-07-07 12:34:15 -0500

    Finalizada la sección de contacto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,30 @@
 function showMenu() {
-    let x = document.getElementById("enlaces")
+    let x = document.getElementById("enlaces");
     if (x.className == ""){
-        x.className += "show"
+        x.className += "show";
     }else{
-        x.className = ""
+        x.className = "";
     }
 }
 function hideMenu() {
-    let x = document.getElementById("enlaces")
-    x.className = ""
+    let x = document.getElementById("enlaces");
+    x.className = "";
+}
+function validacion() {
+    let nombre = document.getElementById("Nombre").value;
+    let correo = document.getElementById("Correo").value;
+    let mensaje = document.getElementById("Mensaje").value;
+    let error = false;
+    if ( nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
+        error = true;
+    }
+    if ( correo == null || correo.length == 0 || /^\s+$/.test(correo) ){
+        error = true;
+    }
+    if ( mensaje == null || mensaje.length == 0 || /^\s+$/.test(mensaje)) {
+        error = true;
+    }
+    if (error == false) {
+        alert("Mensaje Enviado. Gracias por comunicarte conmigo.");
+    }
 }
