422906b195deb449aba99d73484f7bfaf2ab7f49
J4ckDev <j4ckdev@gmail.com>
2020-07-15 11:52:11 -0500

    Realizadas algunas correciones menores

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,12 +17,15 @@ function validacion() {
     let error = false;
     if ( nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
         error = true;
+        alert("Ingrese su nombre y si desea un apellido. Ej. John Doe");
     }
     if ( correo == null || correo.length == 0 || /^\s+$/.test(correo) ){
         error = true;
+        alert("Ingrese un correo valido. Ej. johndoe@email.com");
     }
     if ( mensaje == null || mensaje.length == 0 || /^\s+$/.test(mensaje)) {
         error = true;
+        alert("Escriba su mensaje o inquietud. Ej. Hola, tengo una duda respecto al costo...");
     }
     if (error == false) {
         alert("Mensaje Enviado. Gracias por comunicarte conmigo.");
